8f01c5d96e5a0fb6797aec431dfea3a365c6599d
lasbe <metoo0339@naver.com>
2023-09-06 22:14:39 +0900

    Feat: 예제 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import { useModal } from './lib';
-import { ModalButton } from './lib/react-modal/components/ModalButton';
+import { ModalButton, ModalFooterContainer } from './lib/react-modal';
 
 function App() {
   const { openModal, closeModal } = useModal();
   return (
     <div className="App">
-      <button onClick={() => openModal({ header: 'HI', body: 'BODY', footer: <ModalButton onClick={closeModal}>close</ModalButton> })}>
+      <button
+        onClick={() =>
+          openModal({
+            header: 'HI',
+            body: 'BODY',
+            footer: (
+              <ModalFooterContainer>
+                <ModalButton onClick={closeModal}>close</ModalButton>
+                <ModalButton onClick={closeModal}>close</ModalButton>
+              </ModalFooterContainer>
+            ),
+          })
+        }
+      >
+        open modal
+      </button>
+      <button
+        onClick={() =>
+          openModal({
+            header: 'HI',
+            body: (
+              <div>
+                BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY,
+                BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY,
+                BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY, BODY,
+                <ModalFooterContainer>
+                  <ModalButton onClick={closeModal}>close</ModalButton>
+                  <ModalButton onClick={closeModal}>close</ModalButton>
+                </ModalFooterContainer>
+              </div>
+            ),
+          })
+        }
+      >
         open modal
       </button>
     </div>
